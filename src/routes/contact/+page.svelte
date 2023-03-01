3caf10c6c0b8de4600164dee761fbd3a45f4b57f
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:37:30 -0500

    add content to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,24 @@
 <svelte:head>
     <title>Contacto - Cardano Sin Tecnicismos 2023</title>
 </svelte:head>
-<h1>Contacto</h1>
+
+<div class="container">
+    <h1 class="heading">Contacto</h1>
+    <h2 class="sub-heading">Información importante y canales de comunicación</h2>
+
+    
+</div>
+
+<!-- ## Información importante y canales de comunicación
+
+**¿Prefieres usarWhatsapp?**
+ [https://chat.whatsapp.com/IyxkbkiesL52aiip7NqViL](https://chat.whatsapp.com/IyxkbkiesL52aiip7NqViL)
+
+**Suscríbete a nuestro canal de YouTube** 
+[https://www.youtube.com/channel/UCdYflNYOIZbEzLzSFztGZsA](https://www.youtube.com/channel/UCdYflNYOIZbEzLzSFztGZsA)
+
+**Síguenos en Twitter**
+[https://twitter.com/cardano_st](https://twitter.com/cardano_st)
+
+**Dudas o comentarios**
+cardanost@gmail.com -->

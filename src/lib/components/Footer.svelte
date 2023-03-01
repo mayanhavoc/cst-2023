85ad96be8ecc2195309f8da860c344eaf2c07dde
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:34:34 -0500

    add footer container and media queries

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,6 +5,13 @@
       flex-wrap: wrap;
       justify-content: space-between;
     }
+
+    .footer__container {
+        width: 80%;
+        max-width: 1200px;
+        margin: 0 auto;
+        position: relative;
+    }
     
     ul {
       margin: 0;
@@ -17,21 +24,31 @@
       text-decoration: none;
       color: inherit;
     }
+
+    @media (min-width: 600px) {
+        .row {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    }
 </style>
 
 <footer>
-    <a href="/">Inicio</a>
-    <nav>
-        <ul>
-            <li>
-                <a href="/about">Acerca de</a>
-            </li>
-            <li>
-                <a href="/resources">Recursos</a>
-            </li>
-            <li>
-                <a href="/contact">Contacto</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="footer__container row">
+        <a href="/">Inicio</a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/about">Acerca de</a>
+                </li>
+                <li>
+                    <a href="/resources">Recursos</a>
+                </li>
+                <li>
+                    <a href="/contact">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </footer>

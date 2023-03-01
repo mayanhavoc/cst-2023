22ad1184010a649ba432535fc94246b4ee901e8b
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:34:58 -0500

    add styling and content to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,25 @@
 <svelte:head>
     <title>Acerca de  - Cardano Sin Tecnicismos 2023</title>
 </svelte:head>
-<h1>Acerca de</h1>
+
+<style>
+    .about {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        max-width: 75rem;
+        margin: 0 auto;
+        position: relative;
+    }
+</style>
+
+<div class="container">
+    <h1 class="heading">Acerca del programa</h1>
+    <p class="paragraph">Cardano Sin Tecnicismos busca facilitar espacios en donde usamos <span class="italic">tAda</span> del Grifo testnet para descubrir la multitud de diferentes posibles usos del ecosistema Web3 de Cardano.</p>
+    <p class="small">Nuestro contenido esta orientado a usuario con poca experiencia y/o habilidad técnica así como a entusiastas de la comunidad Cardano en general.</p>
+    <h2 class="sub-heading">¿En qué consiste el programa?</h2>
+    <p class="paragraph">El programa consiste en una serie de talleres en donde aprendemos a usar diferentes herramientas del ecosistema Cardano, desde billeteras digitales hasta delegar nuestro Ada a través del <span>staking</span> y muchos temas más.</p>
+    <p class="paragraph">Nuestro enfoque es presentar el contenido de la manera más sencilla posible, enfocado en el uso práctico de las herramientas sin profundizar en la tecnología subyacente más de lo absolutamente necesario como para que el usuario pueda hacer uso resopnsable de estas herramientas.</p>
+</div>

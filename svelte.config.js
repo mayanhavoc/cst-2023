af1d8e5ab1c58f81c234b1abe71a6924f3d19385
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 14:07:21 -0500

    add netlify deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-netlify';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import sveltePreprocess from 'svelte-preprocess';

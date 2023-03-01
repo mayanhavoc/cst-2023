44a9e4a50032a9dd51ee8024371ea5eddd9bfc43
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:33:14 -0500

    add categories to post

--- src/routes/resources/[slug]/+page.svelte
+++ src/routes/resources/[slug]/+page.svelte
@@ -6,22 +6,40 @@
     <title>Recursos - {data.title}</title>
     <meta property="og:title" content={data.title}/>
 </svelte:head>
-<article>
-    <h1>{data.title}</h1>
-    <p>Published: {data.date}</p>
-    <svelte:component this={data.content} />
-</article>
-{#if data.categories.length}
-<aside>
-    <h2>Posted in:</h2>
-    <ul>
-        {#each data.categories as category }
-            <li>
-                <a href="/resources/category/{category}">
-                    {category}
-                </a>
-            </li>
-        {/each }
-    </ul>
-</aside>
-{/if}
+
+<style>
+     .post {
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+        height: 100%;
+        max-width: 75rem;
+        margin: 5em auto;
+        position: relative;
+    }
+
+    h1, h2, p {
+        margin: 0;
+    }
+</style>
+
+<div class="container">
+    <article>
+        <h1 class="heading">{data.title}</h1>
+        <p class="paragraph">Fecha de publicación: {data.date}</p>
+        <svelte:component this={data.content} />
+        {#if data.categories.length}
+            <h2 class="sub-heading">Categorías</h2>
+            <ul class="list">
+                {#each data.categories as category }
+                    <li class="item">
+                        <a class="item__link" href="/resources/category/{category}">
+                            {category}
+                        </a>
+                    </li>
+                {/each }
+            </ul>
+        
+        {/if}
+    </article>
+</div>

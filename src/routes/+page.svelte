0fd5881737cd7729c9e41aaab369f8fd09951cce
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:31:42 -0500

    center hero container within header and footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
 <svelte:head>
     <title>Cardano Sin Tecnicismos 2023</title>
 </svelte:head>
-<h1>Cardano Sin Tecnicismos 2023</h1>
+
+<style>
+    .hero {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        max-width: 75rem;
+        margin: 0 auto;
+        position: relative;
+    }
+</style>
+
+<div class="hero">
+    <h1 class="heading">Cardano Sin Tecnicismos 2023</h1>
+</div>

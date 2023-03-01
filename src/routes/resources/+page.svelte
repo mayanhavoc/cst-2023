4d806acdc32c4f4ac930a81d563e4c61df68834c
mayanhavoc <mayanhavoc@gmail.com>
2023-03-01 17:34:00 -0500

    format resources page and blog posts

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -2,17 +2,36 @@
     export let data;
 </script>
 
+<style>
+    .recursos {
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+        height: 100%;
+        max-width: 75rem;
+        margin: 5em auto;
+        position: relative;
+    }
+
+    h2, p {
+        margin: 0;
+    }
+</style>
+
 <svelte:head>
     <title>Recursos - Cardano Sin Tecnicismos 2023</title>
 </svelte:head>
-<h1>Recursos</h1>
-<ul>
-    {#each data.posts as post}
-        <li>
-            <h2>
-                <a href={post.path}>{post.meta.title}</a>
-            </h2>
-            <p>Publicado en {post.meta.date}</p>
-        </li>
-    {/each}
-</ul>
+
+<div class="container">
+    <h1 class="heading">Recursos</h1>
+    <ul class="list">
+        {#each data.posts as post}
+            <li class="item">
+                <h2 class="sub-heading">
+                    <a class="item__link" href={post.path}>{post.meta.title}</a>
+                </h2>
+                <p>Publicado en {post.meta.date}</p>
+            </li>
+        {/each}
+    </ul>
+</div>
